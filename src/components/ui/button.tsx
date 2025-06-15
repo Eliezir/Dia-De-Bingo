d4ff92ccddbc2f28e430e1bdbe5248fe9f804012
Eliezir Moreira <eliezir.moreira@run2biz.com>
2025-06-15 04:50:47 -0300

    feat: add bingo image and enhance home page layout with new styles and button variants

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "~/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive cursor-pointer",
   {
     variants: {
       variant: {
@@ -20,6 +20,7 @@ const buttonVariants = cva(
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
+        custom: "group relative inline-flex h-12 items-center justify-center overflow-hidden rounded-md border border-neutral-200 bg-transparent px-6 font-medium text-neutral-600 transition-all [box-shadow:0px_4px_1px_#a3a3a3] active:translate-y-[2px] active:shadow-none",
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
@@ -57,3 +58,5 @@ function Button({
 }
 
 export { Button, buttonVariants }
+
+
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,12 +5,56 @@ export const Route = createFileRoute('/')({
 
 function Home() {
   return (
-    <div className="p-2 flex flex-col items-center justify-center h-screen">
-      <h3 className="text-2xl font-bold">Bem vindo, hoje é dia de bingo!</h3>
-      <p className="text-sm text-muted-foreground">
-        Clique no botão abaixo para criar uma sala
-      </p>
-      {/* <Button>Criar Sala</Button> */}
+    <div className="relative flex flex-col md:flex-row items-center justify-center h-screen bg-gradient-to-br from-[#3c95f1] to-[#c3def9] overflow-hidden">
+      <div
+        className="pointer-events-none absolute right-0 bottom-0 md:top-1/2 md:left-1/2 w-full h-2/3 md:w-2/3 md:h-2/3 opacity-40"
+        style={{
+          backgroundImage:
+            'repeating-linear-gradient(0deg, rgba(255,255,255,0.25) 0, rgba(255,255,255,0.25) 1px, transparent 1px, transparent 32px), repeating-linear-gradient(90deg, rgba(255,255,255,0.25) 0, rgba(255,255,255,0.25) 1px, transparent 1px, transparent 32px)',
+          backgroundPosition: 'center',
+        }}
+      />
+      <div className='flex flex-col md:flex-row items-center justify-center max-w-9xl mx-auto'>
+        <div className="w-full md:w-1/2 flex flex-col items-center justify-center px-6 md:px-16  ">
+          <h1 className="
+            text-5xl
+            md:text-6xl
+            lg:text-8xl
+            font-semibold
+            text-white
+            leading-tight
+            text-shadow-sm
+            text-balance
+            text-center
+          ">
+            Bem-vindo, hoje é dia de <br />
+            <span className="
+              font-bold
+              text-shadow-lg
+              text-7xl
+              md:text-8xl
+              lg:text-9xl
+              block
+              text-center
+              mx-auto
+            ">
+              BINGO!
+            </span>
+          </h1>
+          <div className="flex flex-col justify-center items-center gap-4 mt-4 w-full [&>*]:w-1/3">
+
+          </div>
+
+        </div>
+        <img
+          src="/bingo.png"
+          alt="Mascote do Bingo "
+          className="w-96 md:w-128 drop-shadow-2xl"
+          style={{ maxWidth: '100%', height: 'auto' }}
+        />
+      </div>
     </div>
   )
 }
+
+
